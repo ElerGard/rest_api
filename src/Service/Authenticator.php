cd165bae405575408c10133770a41e46535590a8
ElerGard <norfick@mail.ru>
2022-01-30 02:41:34 +0000

    [7] realy?

---
+++ src/Service/Authenticator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class Authenticator
+{
+
+}
